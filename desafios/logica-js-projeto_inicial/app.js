6b074f6d98a15d4e00ca3d5a1b0e8e41c33ad5de
Ramon Silva <ramonsilvab05@gmail.com>
2025-01-30 10:47:35 -0300

    conclusão do primeiro curso

---
+++ desafios/logica-js-projeto_inicial/app.js
@@ -0,0 +1,47 @@
+// console.log('Boas-vindas!');
+// let nome = 'Ramon';
+// console.log(`Olá ${nome} como posso ajudar?`);
+// let linguagemFavorita = prompt('Qual a linguagem de programação que você mais gosta?');
+
+// let numero1 = 5;
+// let numero2 = 8;
+// let resultado = numero1 + numero2;
+
+// console.log(`O valor da soma entre ${numero1} + ${numero2} é igual a ${resultado}`);
+
+// let idade = prompt('Informe sua idade:');
+
+// if (idade >= 18 ) {
+//     console.log('é maior de idade.');
+// } else {
+//     console.log('é menor de idade.');
+// }
+
+// let numero = prompt('Digite um número:');
+
+// if (numero > 0) {
+//     console.log(`O número ${numero} é positivo!`);
+// } else if (numero == 0) {
+//     console.log(`O número ${numero} é neutro!`);
+// } else {
+//     console.log(`O número ${numero} é negativo!`);
+// }
+
+// let contador = 0;
+
+// while(contador <= 10){
+//     console.log(contador);
+//     contador++;
+// }
+
+// nota = 7;
+
+// if (nota >= 7) {
+//     console.log('Aprovado');
+// } else {
+//     console.log('Reprovado');
+// }
+
+let numeroAleatorio = parseInt(Math.random() * 1000 + 1);
+
+console.log(numeroAleatorio);
--- projetos/logica-js-projeto_inicial/app.js
+++ projetos/logica-js-projeto_inicial/app.js
@@ -1,10 +1,35 @@
+
+let valorMaximo = 1000;
+let numeroSecreto = parseInt(Math.random() * valorMaximo + 1);
+console.log(numeroSecreto);
 alert('Bem vindos ao jogo do número secreto');
-let numeroSecreto = 5;
-let chute = prompt('escolha um número entre 1 e 10');
 
-if (chute == numeroSecreto) {
+let chute;
+let tentativas = 1;
 
-    alert('Parabéns você descobriu o número secreto!');
-} else {
-    alert('você errou recarrege a página e tente novamente!')
+while (chute != numeroSecreto) {
+
+    chute = prompt(`escolha um número entre 1 e ${valorMaximo}`);
+
+    if (chute == numeroSecreto) {
+        break;
+
+    } else {
+        if (numeroSecreto > chute) {
+            alert (`O número secreto é maior que ${chute}`);
+        } else {
+            alert (`O número secreto é menor que ${chute}`);
+        }
+
+        tentativas++;
+    }
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Parabéns você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+
+// if (tentativas > 1) {
+//     alert(`Parabéns você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+// } else {
+//     alert(`Parabéns você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+// }
