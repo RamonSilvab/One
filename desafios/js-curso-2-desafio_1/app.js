e4479c17ae96d7c781f832c8e2924d1410e8cdb8
Ramon Silva <ramonsilvab05@gmail.com>
2025-01-30 11:39:23 -0300

    interagino javaScript com html

---
+++ desafios/js-curso-2-desafio_1/app.js
@@ -0,0 +1,25 @@
+let titulo = document.querySelector('h1');
+
+titulo.innerHTML = 'Hora do Desafio';
+
+function consoleTeste() {
+    console.log('O botão foi clicado');
+}
+
+function alertTeste() {
+    alert('Eu amo JS');
+}
+
+function promptTeste() {
+    let cidade = prompt('Qual o nome da cidade aonde você mora?');
+
+    alert(`Estive em ${cidade} e lembrei de você!`);
+}
+
+function somaTeste() {
+    let n1 = parseInt(prompt('Digite um número inteiro:'));
+    let n2 = parseInt(prompt('Digite outro número inteiro:'));
+    let soma = n1 + n2;
+
+    alert(`A soma entre ${n1} + ${n2} é igual a ${soma}!`)
+}
