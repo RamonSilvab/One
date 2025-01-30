29d03ae6d9b0909201507d6dd683cae83b35acc3
Ramon Silva <ramonsilvab05@gmail.com>
2025-01-30 15:02:43 -0300

    finalizando o projeto do curso 2

--- desafios/js-curso-2-desafio_1/app.js
+++ desafios/js-curso-2-desafio_1/app.js
@@ -1,25 +1,33 @@
-let titulo = document.querySelector('h1');
+// let titulo = document.querySelector('h1');
 
-titulo.innerHTML = 'Hora do Desafio';
+// titulo.innerHTML = 'Hora do Desafio';
 
-function consoleTeste() {
-    console.log('O botão foi clicado');
+// function consoleTeste() {
+//     console.log('O botão foi clicado');
+// }
+
+// function alertTeste() {
+//     alert('Eu amo JS');
+// }
+
+// function promptTeste() {
+//     let cidade = prompt('Qual o nome da cidade aonde você mora?');
+
+//     alert(`Estive em ${cidade} e lembrei de você!`);
+// }
+
+// function somaTeste() {
+//     let n1 = parseInt(prompt('Digite um número inteiro:'));
+//     let n2 = parseInt(prompt('Digite outro número inteiro:'));
+//     let soma = n1 + n2;
+
+//     alert(`A soma entre ${n1} + ${n2} é igual a ${soma}!`)
+// }
+
+function olaMundo(n1) {
+    console.log(n1 * n1);
 }
 
-function alertTeste() {
-    alert('Eu amo JS');
-}
+let numero1 = prompt('Digite um número:');
 
-function promptTeste() {
-    let cidade = prompt('Qual o nome da cidade aonde você mora?');
-
-    alert(`Estive em ${cidade} e lembrei de você!`);
-}
-
-function somaTeste() {
-    let n1 = parseInt(prompt('Digite um número inteiro:'));
-    let n2 = parseInt(prompt('Digite outro número inteiro:'));
-    let soma = n1 + n2;
-
-    alert(`A soma entre ${n1} + ${n2} é igual a ${soma}!`)
-}
+olaMundo(numero1);
--- projetos/js-curso-2-aula1/app.js
+++ projetos/js-curso-2-aula1/app.js
@@ -1,9 +1,68 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+let listaDeNumerosSorteados = [];
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10.';
+function exibeTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate: 1.2});
+}
+
+function exibirMensagemInicial() {
+    
+    exibeTextoNaTela('h1', 'Jogo do número secreto');
+    exibeTextoNaTela('p', 'Escolha um número entre 1 e 10.');
+}
+
+exibirMensagemInicial();
+
 
 function verificarChute() {
-    console.log('O botão foi clicado.')
+    let chute = document.querySelector('input').value;
+    if (chute == numeroSecreto) {
+        exibeTextoNaTela('h1', 'Acertou!');
+        let tentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${tentativa}!`;
+        exibeTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if(chute > numeroSecreto) {
+            exibeTextoNaTela('p', `O número secreto é menor que ${chute}.`);
+        } else {
+            exibeTextoNaTela('p', `O número secreto é maior que ${chute}.`);
+        }
+
+        tentativas++;
+        limparCampo();
+    }
+}
+
+function gerarNumeroAleatorio() {
+    let numeroEscolhido = parseInt(Math.random() * 10 + 1);
+    let quantidadeDeNumerosNaLista = listaDeNumerosSorteados.length;
+
+    if (quantidadeDeNumerosNaLista == 10) {
+        listaDeNumerosSorteados = [];
+    }
+
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        console.log(listaDeNumerosSorteados);
+        return numeroEscolhido
+    }
+}
+
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
 }
